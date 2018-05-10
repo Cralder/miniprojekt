041811309710a35d39b1d9bb2318ec135973007e
Cralder <jesper_palmer@hotmail.com>
2018-05-10 15:26:00 +0200

    påbörjade projekt
    lade till inläsning av två filer från kommandraden och jämför med javakeywords

--- src/miniprojekt/Miniprojekt.java
+++ src/miniprojekt/Miniprojekt.java
@@ -1,12 +1,47 @@
 package miniprojekt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Hashtable;
+import java.util.Scanner;
+
 public class Miniprojekt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Hello
-		System.out.println("Hello world");
-		//Hahaeksde
+		
+		Hashtable<String, Integer> table = new Hashtable<String, Integer>();
+		
+		try {
+			Scanner keywords = new Scanner(new File("src\\miniprojekt\\javanyckelord.txt"));
+			Scanner cmd = new Scanner(System.in);
+			Scanner in1 = new Scanner(new File(cmd.next()));
+			Scanner in2 = new Scanner(new File(cmd.next()));
+			cmd.close();
+			System.out.println(keywords.next());
+			
+			String current;
+			String keyword;
+			
+			while(in1.hasNext()) {
+				current = in1.next();
+				
+				while(keywords.hasNext()) {
+					keyword = keywords.next();
+					
+					if(current.equals(keyword)) {
+						table.put(keyword, 1);
+					}
+				}
+			}
+			
+			in1.close();
+			in2.close();
+			keywords.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
